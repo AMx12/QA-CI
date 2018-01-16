9c5dba6d566bac05a3235b49bb871ee925d887d5
Andrew Mooney <AMx12@users.noreply.github.com>
2018-01-16 14:42:22 +0000

    wuut

---
+++ CI-QA/src/main/java/Andrew/Mooney/CI_QA/helloWorld.java
@@ -0,0 +1,11 @@
+package Andrew.Mooney.CI_QA;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+	}
+
+}
